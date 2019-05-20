f7585906564cf19b3fbb6b94841dbe744fe1509b
Mohd Asim Khan <46257479+asimunit@users.noreply.github.com>
2019-05-20 17:05:49 +0530

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+
+import np_writter
+
+name = input("Enter your name : ")
+
+cap = cv2.VideoCapture(0)
+
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+f_list = []
+
+while True:
+
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = classifier.detectMultiScale(gray, 1.5, 5)
+
+    faces = sorted(faces, key=lambda f: f[2]*f[3], reverse=True)
+
+    faces = faces[:1]
+
+    if len(faces) == 1:
+        face = faces[0]
+        x, y, w, h = face
+        im_face = frame[y:y+h, x:x+w]
+        cv2.imshow("face", im_face)
+
+
+    # if not ret:
+    #     continue
+
+    cv2.imshow("full", frame)
+
+    key = cv2.waitKey(1)
+
+    if key & 0xFF == ord('q'):
+        break
+    elif key & 0xFF == ord('c'):
+        if len(faces) == 1:
+            gray_face = cv2.cvtColor(im_face, cv2.COLOR_BGR2GRAY)
+            gray_face = cv2.resize(gray_face, (100, 100))
+            print(len(f_list), type(gray_face), gray_face.shape)
+            f_list.append(gray_face.reshape(-1))
+        else:
+            print("face not found")
+
+        if len(f_list) == 10:
+            break
+
+
+np_writter.write(name, np.array(f_list))
+
+# np.save(name, np.array(f_list))
+
+cap.release()
